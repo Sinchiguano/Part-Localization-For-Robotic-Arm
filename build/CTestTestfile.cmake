4e56fc74f166fcbd4119aaee578214bb4ba35bb9
Sinchiguano <cesarsinchiguano@hotmail.es>
2018-11-26 23:24:39 +0100

    cali

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,16 +5,14 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(ros_astra_launch)
+subdirs(astra/ros_astra_launch)
 subdirs(multimaster_fkie/multimaster_fkie)
 subdirs(multimaster_fkie/multimaster_msgs_fkie)
 subdirs(multimaster_fkie/default_cfg_fkie)
 subdirs(multimaster_fkie/master_discovery_fkie)
 subdirs(multimaster_fkie/master_sync_fkie)
 subdirs(multimaster_fkie/node_manager_fkie)
-subdirs(ros_astra_camera)
-subdirs(object_recognition_core)
-subdirs(ork_tutorials)
+subdirs(astra/ros_astra_camera)
 subdirs(yumi/gazebo_mimic)
 subdirs(realsense/realsense2_camera)
 subdirs(panda_moveit_config)
---
+++ build/astra/ros_astra_camera/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/casch/yumi_ws/src/astra/ros_astra_camera
+# Build directory: /home/casch/yumi_ws/build/astra/ros_astra_camera
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
